572f5e0b61c892580ff307d8c8e8868bfb928d4f
Johanna Monroy <johamonroy20@gmail.com>
2026-06-11 11:07:13 +0200

    feat: implement turn cycle between player and machine in Game

--- src/main/java/tictactoe/Game.java
+++ src/main/java/tictactoe/Game.java
@@ -3,4 +3,20 @@ package tictactoe;
 //Lógica de la partida ( estado de las fichas)
 //Lógica estado final de la partida (ganador, perdedor, empate)
 public class Game {
+    private Board board;
+    private Display display;
+    private Turn turn ;
+
+    public Game(Board board, Display display, Turn turn) {
+        this.board = board;
+        this.display = display;
+        this.turn = turn;
+    }
+    public void play() {
+        Player current = turn.getCurrentPlayer();
+        System.out.println("Turno de: " + current.getName());
+        current.move(board);
+        display.printBoard(board.getBoard());
+        turn.switchTurn();
+    }
 }
